946c6f4763c45098e1e4f78cd0c04f6163eefe3f
abdullahi-mohammed <moh.abdullahi2003@gmail.com>
2025-12-30 02:20:29 +0100

    .

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f001acab'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/pwa.html",
-    "revision": "0.6v3f60vdkn"
+    "revision": "0.h2fcj0b9j3"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/pwa.html"), {
