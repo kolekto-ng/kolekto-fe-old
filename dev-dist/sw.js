0465fa8d960db8ac79cef41df490caf7b3e4b726
gazalian <gazaliantech@gmail.com>
2026-04-03 14:50:54 +0100

    full update from main branch

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f001acab'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/pwa.html",
-    "revision": "0.5uvuvp2lajg"
+    "revision": "0.evr91enadko"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/pwa.html"), {
